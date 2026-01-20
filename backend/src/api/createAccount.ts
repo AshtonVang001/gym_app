dcfecc251ce0d17859f2863c7167d883f633f691
Ashton Vang <ashtonvang100@gmail.com>
2026-01-20 14:51:01 -0800

    test: check package.json commits

---
+++ backend/src/api/createAccount.ts
@@ -0,0 +1,12 @@
+import { app } from "../index.js";
+import { dbConfig } from "./dbconnect.js";
+import * as bcrypt from "bcrypt"
+import "dotenv/config"
+
+
+//hash password
+//user will enter username, email, and password
+
+app.post("create-account", async (c) => {
+    
+})
