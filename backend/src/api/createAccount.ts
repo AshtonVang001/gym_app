82742c79c9033b09ff4234192b7a77c4c0f05f2c
Ashton Vang <ashtonvang100@gmail.com>
2026-01-20 17:22:31 -0800

    add: add handler and route for creating an account

--- backend/src/api/createAccount.ts
+++ backend/src/api/createAccount.ts
@@ -1,12 +1,52 @@
 import { app } from "../index.js";
 import { dbConfig } from "./dbconnect.js";
-import * as bcrypt from "bcrypt"
-import "dotenv/config"
-
+import * as bcrypt from "bcrypt";
+import "dotenv/config";
 
 //hash password
 //user will enter username, email, and password
 
 app.post("create-account", async (c) => {
-    
-})
+  const saltRounds = 10;
+
+  try {
+    const { username, email, password } = await c.req.json();
+
+    const hashedPassowrd = await bcrypt.hash(password, saltRounds);
+
+    //insert into db here
+
+    const user =
+      await dbConfig`INSERT INTO users (username, email, password, role, email_verified, status, profile_completed, refresh_token)
+                VALUES (${username}, ${email}, ${hashedPassowrd}, user, ${false}, online, test)
+                RETURNING id, username, email, created_at`;
+
+    if (user.length > 0) {
+      return c.json(
+        {
+          success: true,
+          message: "Account created successfully!",
+          data: user[0],
+        },
+        201,
+      );
+    } else {
+      return c.json(
+        {
+          success: false,
+          message: "Account creation failed",
+        },
+        400,
+      );
+    }
+  } catch (error) {
+    console.error("Error createding account: ", error);
+    return c.json(
+      {
+        success: false,
+        message: `Account creation failed. Error: ${error}`,
+      },
+      500,
+    );
+  }
+});
